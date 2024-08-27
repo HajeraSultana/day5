67a3c3b2650013868b04771df582cd9412f2ea9b
HajeraSultana <e031306@gmail.com>
2024-08-27 23:50:58 +0100

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,16 @@
+print ("FRIENDS Character Creator")
+print()
+who = input("Are you Very Awkward At Times? ")
+if who == "No":
+  print("Then you're not Ross")
+Who = input("Neat And Tidy & Also Overbearing? ")
+if Who == "No":
+  print("No, you're not Monica")
+wHo = input("Are you Funny And Always Happy? ")
+if wHo == "No":
+  print("Aww, You're not Joey")
+whO = input ("Are you Extremely Witty & Annoyingly Sarcastic? ")
+if whO == "No":
+  print("You are not Chandler Bing")
+else:
+  print("Hello Chandler Bing")
